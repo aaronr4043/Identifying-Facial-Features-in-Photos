bf5b0f4d7430d2b7a4718bc8ccf8bf3f70424651
Gytis Mackevicius <gytismack@gmail.com>
2017-11-01 21:26:18 +0000

    Scaling
    Upscaling and downscaling of the image to accomodate the algorithm with better reads on the ROI

--- Haar.py
+++ Haar.py
@@ -8,12 +8,15 @@ from matplotlib import image as image
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
  
 img = cv2.imread('graduation.jpg')
+img = cv2.resize(img,None,fx=5, fy=5, interpolation = cv2.INTER_CUBIC) #Upscaling the image to accomodate Haar's
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
 	cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 	
+small = cv2.resize(img, (0,0), fx=0.3, fy=0.3) #Resizing the image to fit the screen
+
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
