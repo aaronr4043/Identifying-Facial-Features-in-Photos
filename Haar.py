1b9e8497c7c312b9faa3bfa10281d661e8f93db7
JayH117 <C14403052@mydit.ie>
2017-11-06 23:45:05 +0000

    Comments done

--- Haar.py
+++ Haar.py
@@ -18,8 +18,9 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
 	cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),10)
-	
-small = cv2.resize(img,(width,height)) #Resizing the image to fit the screen
+
+# Resizing image to original size
+small = cv2.resize(img,(width,height)) 
 
 cv2.imshow('img',small)
 cv2.waitKey(0)
